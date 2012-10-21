164d42c9b25e6db495733561cfed9f058540bc73
sirMackk <c.tairix@gmail.com>
2012-10-21 12:10:29 -0400

    Working on modularizing code through classes

--- reise/test_proxy3.py
+++ reise/test_proxy3.py
@@ -6,17 +6,6 @@ import socket
 import Queue
 import time
 from sys import argv
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp.bind(('127.0.0.1', 8080))
-tcp.listen(10)
-#included: client.py
-#first experiment with a tcp localhost proxy coupled with a send/recv udp interface
-#simple initial test work
-#must work on client to make it into an internet facing proxy
-#must also work out issues with threads
-
-
 #TODO 
 #- make this whole program a class, allowing for easy implementation 
 #in other projects, including the project that's the purpose of this experiment.
@@ -25,12 +14,69 @@ tcp.listen(10)
 
 
 
+class tcpProxy(object):
+    def __init__(self, target, port, threads, l4):
+        self._l4 = l4
+        self._tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #gets local IP by getfqdn, might be buggy though, giving IP of wrong interface
+        self._tcp.bind((socket.gethostbyname(socket.getfqdn()), port))
+        self._tcp.listen(10)
+        #basically, each thread should call either outbound_target or outbound_global
+        #to handle outgoing connections. target is when a specific host is required and
+        #global is when the tcpProxy object is just supposed send out http packets
+        #to their normal destionations.
+        
+        #have to decide which function to use, or maybe consolidate them into one
+        #function. Might even move outbound into ClientThread class. Gotta think of a 
+        #better structure for this class.
+
+        #This kind of approach will allow to easily create tcp servers to handle
+        #various kinds of traffic that can either be the start point, middle point, or
+        #endpoint of a proxy connection.
+
+
+        clientPool = Queue.Queue()
+        for i in range(5):
+            ClientThread().start()
+        while 1:
+            clientPool.put(tcp.accept())
+
+
+tcp.close()
+
+    def outbound_target(self, data):
+        if self._l4.lower() == 'udp':
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.sendto(data, target)
+            return recv_udp(s)
+        elif self._l4.lower() == 'tcp':
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target, 80))
+            s.sendall(data)
+            data = recv_data(s)
+            s.close()
+            return data
+
+    def outbound_global(self, data):
+        if self._l4.lower() == 'udp':
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.sendto(data, target)
+            return recv_udp(s)
+        elif self._l4.lower() == 'tcp':
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.getIP(data), 80))
+            s.sendall(data)
+            data = recv_data(s)
+            s.close()
+            return data
+
+
+
 
 #ClientThread class subclasses threading.Thread class
 class ClientThread(threading.Thread):
     def run(self):
         while 1:
-            
             print '[starting thread]'
             try:
                 item = clientPool.get()
@@ -121,13 +167,4 @@ class ClientThread(threading.Thread):
         
         return ''.join(data)
 
-script, target = argv
 
-clientPool = Queue.Queue()
-for i in range(1):
-    ClientThread().start()
-while 1:
-    clientPool.put(tcp.accept())
-
-
-tcp.close()
