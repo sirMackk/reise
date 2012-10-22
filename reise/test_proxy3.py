4a7db4682a208415b1d63bd96706157a8a25eaa0
sirMackk <c.tairix@gmail.com>
2012-10-22 09:26:56 -0400

    Further work on modularizing the code

--- reise/test_proxy3.py
+++ reise/test_proxy3.py
@@ -7,10 +7,17 @@ import Queue
 import time
 from sys import argv
 #TODO 
-#- make this whole program a class, allowing for easy implementation 
-#in other projects, including the project that's the purpose of this experiment.
 #- initial testing shows that this script uses about 30% of the CPU, so it would
 #be nice to think about optimizing it later. Maybe Cython?
+#- think about using getopt instead of just argv, might make things easier and nicer.
+
+#To make this class more universal, I should let the user be able to chose source
+#and destination ports. By default they could be 80 and 53. I must also get a resource
+#to forge dns packets along with being able to fragment them as normal dns packet size is
+#512 bytes whereas normal http packets size is about 1500 bytes. Main issue now is
+#to make this project highly reusable. Best idea so far is have some default values
+#that can be overridden in the script. Will this impact the performance?
+#Gotta think about the general tcpProxy class structure.
 
 
 
@@ -21,61 +28,25 @@ class tcpProxy(object):
         #gets local IP by getfqdn, might be buggy though, giving IP of wrong interface
         self._tcp.bind((socket.gethostbyname(socket.getfqdn()), port))
         self._tcp.listen(10)
-        #basically, each thread should call either outbound_target or outbound_global
-        #to handle outgoing connections. target is when a specific host is required and
-        #global is when the tcpProxy object is just supposed send out http packets
-        #to their normal destionations.
+
+
+
+
         
-        #have to decide which function to use, or maybe consolidate them into one
-        #function. Might even move outbound into ClientThread class. Gotta think of a 
-        #better structure for this class.
-
-        #This kind of approach will allow to easily create tcp servers to handle
-        #various kinds of traffic that can either be the start point, middle point, or
-        #endpoint of a proxy connection.
-
-
+#Doesn't work, just a place-holder
+    def start(self):
         clientPool = Queue.Queue()
         for i in range(5):
-            ClientThread().start()
+            ClientThread().start(l4, target)
         while 1:
             clientPool.put(tcp.accept())
 
-
-tcp.close()
-
-    def outbound_target(self, data):
-        if self._l4.lower() == 'udp':
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.sendto(data, target)
-            return recv_udp(s)
-        elif self._l4.lower() == 'tcp':
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target, 80))
-            s.sendall(data)
-            data = recv_data(s)
-            s.close()
-            return data
-
-    def outbound_global(self, data):
-        if self._l4.lower() == 'udp':
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.sendto(data, target)
-            return recv_udp(s)
-        elif self._l4.lower() == 'tcp':
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.getIP(data), 80))
-            s.sendall(data)
-            data = recv_data(s)
-            s.close()
-            return data
-
-
-
+        tcp.close()
 
 #ClientThread class subclasses threading.Thread class
 class ClientThread(threading.Thread):
-    def run(self):
+    def run(self, l4, target):
+    connection = {'tcp': connect_tcp, 'udp': connect_udp}
         while 1:
             print '[starting thread]'
             try:
@@ -90,13 +61,7 @@ class ClientThread(threading.Thread):
             msg, addr = item
             buffer = msg.recv(8192)   
             print 'Buffer length: %d' % len(buffer)
-            out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            #out.bind((socket.gethostbyname(socket.getfqdn()), 53))
-            print 'outbound socket acquired, connecting...'
-            print 'sending buffer to target'
-            out.sendto(buffer, (target.split(':')[0], int(target.split(':')[1]))) 
-            print 'receiving...'
-            data = self.recv_udp(out)
+            connection[l4](buffer, target)            
             print 'got reply, closing socket, sending back to localhost'
             print data
             msg.sendall(data)
@@ -167,4 +132,26 @@ class ClientThread(threading.Thread):
         
         return ''.join(data)
 
+    def connect_tcp(self, data, ip):
+        if ip is None:
+            ip = self.getIP(data)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #make port mutable
+        s.connect((ip, 80))
+        s.sendall(data)
+        data = recv_data(s)
+        s.close()
+        return data
 
+    def connect_udp(self, data, ip):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #make port mutable
+        s.sendto(data, (ip, 53))
+        return recv_udp(s)
+
+
+#example code, place-holder
+if name == '__main__':
+    script, target, port, threads, protocol = argv
+    proxy = tcpProxy(target, port, threads, protocol)
+    proxy.run()
